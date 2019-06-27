c70b3da6e2b16c0f173db9b406952460c9ed457d
raredown <raredown@gmail.com>
2019-06-27 16:12:59 +0200

    Creando proyecto para arquitectura nueva

---
+++ Arquitectura/src/main/java/es/iecisa/ArquitecturaApplication.java
@@ -0,0 +1,13 @@
+package es.iecisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturaApplication.class, args);
+	}
+
+}
---
+++ Arquitectura/src/test/java/es/iecisa/ArquitecturaApplicationTests.java
@@ -0,0 +1,16 @@
+package es.iecisa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArquitecturaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
