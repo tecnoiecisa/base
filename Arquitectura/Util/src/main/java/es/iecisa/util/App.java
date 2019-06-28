f5e58ee64bb2482c0d2bea7178e9834ae7608dc6
raredown <raredown@gmail.com>
2019-06-28 09:19:53 +0200

    Modularizando el proyecto en varios modulos

---
+++ Arquitectura/Util/src/main/java/es/iecisa/util/App.java
@@ -0,0 +1,16 @@
+package es.iecisa.util;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
